874a98b7455ffae602aaf7106ffb72cfc319f66d
Katia Nienow <nienowkatia@gmail.com>
2023-11-03 17:42:35 -0300

    requisitos 04 e 05 - implementando a classe GerenciamentoVotacao, com seus atributos, e implementando os métodos de cadastro de pessoas candidatas e eleitoras da interface GerenciamentoVotacaoInterface na classe GerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,6 +1,44 @@
 package com.betrybe.sistemadevotacao;
 
-/** Classe GerenciamentoVotacao. */
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+/** Classe GerenciamentoVotacao. */
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<PessoaCandidata>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<PessoaEleitora>();
+  private ArrayList<String> cpfsComputados = new ArrayList<String>();
+
+  /** Método para cadastrar pessoas candidatas. */
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+      if (pessoaCandidata.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+      }
+    }
+    PessoaCandidata candidata = new PessoaCandidata(nome, numero);
+    pessoasCandidatas.add(candidata);
+  }
+
+  /** Método para cadastrar pessoas eleitoras. */
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    for (PessoaEleitora pessoaEleitora : pessoasEleitoras) {
+      if (pessoaEleitora.getCpf() == cpf) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+      }
+    }
+    PessoaEleitora eleitora = new PessoaEleitora(nome, cpf);
+    pessoasEleitoras.add(eleitora);
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+    throw new UnsupportedOperationException("Unimplemented method 'votar'");
+  }
+
+  @Override
+  public void mostrarResultado() {
+    throw new UnsupportedOperationException("Unimplemented method 'mostrarResultado'");
+  }
 }
--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -3,10 +3,10 @@ package com.betrybe.sistemadevotacao;
 /** Classe Principal. */
 public class Principal {
 
+  /** Classe Main. */
   public static void main(String[] args) {
     new PessoaCandidata(null, 0);
     new PessoaEleitora(null, null);
-
   }
 
 }
